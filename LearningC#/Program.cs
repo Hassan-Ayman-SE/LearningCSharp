40f61adc48e3d3f0485e283bd436433391bd4b70
Hassan Karraz <24042732@student.ltuc.com>
2024-06-04 22:25:29 +0300

    Learning variables

--- LearningC#/Program.cs
+++ LearningC#/Program.cs
@@ -4,10 +4,39 @@
     {
         static void Main(string[] args)
         {
+            //V 1 + 2
             Console.WriteLine("Hello, World!");
             Console.WriteLine("My Name is Hassan");
             Console.Beep();
             //This is a comment in c#
+
+            //V 3
+
+            int x; // declaration
+            x = 123; // initialization
+
+            int y = 321; // declaration + initialization
+
+            int z = x + y;
+
+            int age = 21; // whole integer
+            double height = 174.5; // decimal number
+            bool alive = true; //true or false
+            char symbol = '@'; // single character
+            String name = "Hassan"; // a series of characters
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Your age is " + age);
+            Console.WriteLine("Your height is " + height + "cm");
+            Console.WriteLine("Are you alive? " + alive);
+            Console.WriteLine("Your symbol is: " + symbol);
+
+            String userName = symbol + name;
+
+            Console.WriteLine("Your username is: " + userName);
+
+
+
             Console.ReadKey();
         }
     }

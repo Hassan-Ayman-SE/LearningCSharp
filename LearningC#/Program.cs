6423a2c95ed9a327c2738fc988545fad657b4137
Hassan Karraz <24042732@student.ltuc.com>
2024-06-04 22:27:25 +0300

    constant value

--- LearningC#/Program.cs
+++ LearningC#/Program.cs
@@ -35,7 +35,16 @@
 
             Console.WriteLine("Your username is: " + userName);
 
+            // V 4
 
+            // constants  = immutable values which are known at compile time
+            //              and do not change for the life of the program
+
+            const double pi = 3.14;
+
+            //pi = 420; //can't change this constant
+
+            Console.WriteLine(pi);
 
             Console.ReadKey();
         }

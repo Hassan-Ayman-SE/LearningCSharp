f1980660b3b05e3c7479ab4500f95b0c45b47384
Hassan Karraz <24042732@student.ltuc.com>
2024-06-05 12:12:03 +0300

    String methods

--- LearningC#/Program.cs
+++ LearningC#/Program.cs
@@ -147,6 +147,28 @@
 
             //Console.WriteLine("The hypotenuse is: " + c);
 
+            //V 11 
+
+            String fullName = "Hassan Karraz";
+            String phoneNumber = "123-456-7890";
+
+            //fullName = fullName.ToUpper();
+            //fullName = fullName.ToLower();
+            //Console.WriteLine(fullName);
+
+            //phoneNumber = phoneNumber.Replace("-","");
+            //Console.WriteLine(phoneNumber);
+
+            //String userName = fullName.Insert(0,"Mr.");
+            //Console.WriteLine(userName);
+
+            //Console.WriteLine(fullName.Length);
+
+            String firstName = fullName.Substring(0, 6);
+            String lastName = fullName.Substring(7);
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
 
 
             Console.ReadKey();

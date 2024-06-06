b76743a52728dacc4e9f8a13529990113bf30f55
Hassan Karraz <24042732@student.ltuc.com>
2024-06-06 14:46:20 +0300

    string interpolation

--- LearningC#/Program.cs
+++ LearningC#/Program.cs
@@ -666,6 +666,17 @@
 
             //===============================================================================
             // V 29
+            // string interpolation = allows us to insert variables into a string literal
+            //                        precede a string literal with $
+            //                        {} are placeholders
+
+            String firstName = "Bro";
+            String lastName = "Code";
+            int age = 21;
+
+            Console.WriteLine($"Hello {firstName} {lastName}.");
+            Console.WriteLine($"You are {age,-10} old.");
+
 
 
 

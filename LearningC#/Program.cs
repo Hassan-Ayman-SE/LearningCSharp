20dbee976ce960d4fd1e2e670d4f1c88e04463ee
Hassan Karraz <24042732@student.ltuc.com>
2024-06-06 14:16:57 +0300

    Arrays

--- LearningC#/Program.cs
+++ LearningC#/Program.cs
@@ -503,6 +503,22 @@
 
             Console.WriteLine("Bye!");
 
+            //V 21
+            // array = a variable that can store multiple values. fixed size
+
+            //String[] cars = {"BMW", "Mustang", "Corvette"};
+
+            String[] cars = new string[3];
+
+            cars[0] = "Tesla";
+            cars[1] = "Mustang";
+            cars[2] = "Corvette";
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+
 
             Console.ReadKey();
         }

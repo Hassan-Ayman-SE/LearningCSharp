154df5e49bd658ac8316fe610b1ed97ac0abf1ea
Hassan Karraz <24042732@student.ltuc.com>
2024-06-07 15:57:36 +0300

    Objects

---
+++ LearningC#/Human.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningC_
+{
+    class Human
+    {
+        public String name;
+        public int age;
+
+        public void Eat()
+        {
+            Console.WriteLine(name + " is eating");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine(name + " is sleeping");
+        }
+    }
+
+}
--- LearningC#/Program.cs
+++ LearningC#/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LearningC_
+﻿using System.Security.Cryptography;
+
+namespace LearningC_
 {
     internal class Program
     {
@@ -719,9 +721,24 @@
 
             //===============================================================================
             // V 32
+            // object = An instance of a class
+            //          A class can be used as a blueprint to create objects (OOP)
+            //          objects can have fields & methods (characteristics & actions)
 
+            Human human1 = new Human();
+            Human human2 = new Human();
 
+            human1.name = "Rick";
+            human1.age = 65;
 
+            human2.name = "Morty";
+            human2.age = 16;
+
+            human1.Eat();
+            human1.Sleep();
+
+            human2.Eat();
+            human2.Sleep();
 
             //===============================================================================
             // V 33

801de44b33ca09728292b4d08b4c3a7ae6aef2ba
Hassan Karraz <24042732@student.ltuc.com>
2024-06-09 11:42:15 +0300

    generic

--- LearningC#/Program.cs
+++ LearningC#/Program.cs
@@ -1084,60 +1084,80 @@ namespace LearningC_
             //Console.WriteLine(Planets.Mercury + " is planet #" + (int)Planets.Mercury);
             //Console.WriteLine(Planets.Pluto + " is planet #" + (int)Planets.Pluto);
 
-        //    String name = PlanetRadius.Earth.ToString();
-        //    int radius = (int)PlanetRadius.Earth;
-        //    double volume = Volume(PlanetRadius.Earth);
+            //    String name = PlanetRadius.Earth.ToString();
+            //    int radius = (int)PlanetRadius.Earth;
+            //    double volume = Volume(PlanetRadius.Earth);
 
-        //    Console.WriteLine("planet: " + name);
-        //    Console.WriteLine("radius: " + radius + "km");
-        //    Console.WriteLine("volume: " + volume + "km^3");
-
-
-        //    public static double Volume(PlanetRadius radius)
-        //    {
-        //        double volume = (4.0 / 3.0) * Math.PI * Math.Pow((int)radius, 3);
-        //        return volume;
-        //    }
-
-        //        enum Planets
-        //{
-        //    Mercury = 1,
-        //    Venus = 2,
-        //    Earth = 3,
-        //    Mars = 4,
-        //    Jupiter = 5,
-        //    Saturn = 6,
-        //    Uranus = 7,
-        //    Neptune = 8,
-        //    Pluto = 9
-        //}
-
-        //enum PlanetRadius
-        //{
-        //    Mercury = 2439,
-        //    Venus = 6051,
-        //    Earth = 6371,
-        //    Mars = 3389,
-        //    Jupiter = 69911,
-        //    Saturn = 58232,
-        //    Uranus = 25362,
-        //    Neptune = 24622,
-        //    Pluto = 1188
-        //}
+            //    Console.WriteLine("planet: " + name);
+            //    Console.WriteLine("radius: " + radius + "km");
+            //    Console.WriteLine("volume: " + volume + "km^3");
 
 
+            //    public static double Volume(PlanetRadius radius)
+            //    {
+            //        double volume = (4.0 / 3.0) * Math.PI * Math.Pow((int)radius, 3);
+            //        return volume;
+            //    }
 
-        //===============================================================================
-        // V 49
+            //        enum Planets
+            //{
+            //    Mercury = 1,
+            //    Venus = 2,
+            //    Earth = 3,
+            //    Mars = 4,
+            //    Jupiter = 5,
+            //    Saturn = 6,
+            //    Uranus = 7,
+            //    Neptune = 8,
+            //    Pluto = 9
+            //}
+
+            //enum PlanetRadius
+            //{
+            //    Mercury = 2439,
+            //    Venus = 6051,
+            //    Earth = 6371,
+            //    Mars = 3389,
+            //    Jupiter = 69911,
+            //    Saturn = 58232,
+            //    Uranus = 25362,
+            //    Neptune = 24622,
+            //    Pluto = 1188
+            //}
 
 
 
-        //===============================FINISH================================================
-        // V 50
+            //===============================================================================
+            // V 49
+            // generic =  "not specific to a particular data type"
+            //            add <T> to: classes, methods, fields, etc.
+            //            allows for code reusability for different data types
+
+            //int[] intArray = { 1, 2, 3 };
+            //double[] doubleArray = { 1.0, 2.0, 3.0 };
+            //String[] stringArray = { "1", "2", "3" };
+
+            //displayElements(intArray);
+            //displayElements(doubleArray);
+            //displayElements(stringArray);
+
+            //public static void displayElements<Thing>(Thing[] array)
+            //{
+            //    foreach (Thing item in array)
+            //    {
+            //        Console.Write(item + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
 
 
 
-        Console.ReadKey();
+            //===============================FINISH================================================
+            // V 50
+
+
+
+            Console.ReadKey();
         }
     }
 }
